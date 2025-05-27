bbc2da691e78f448a25123881421bce749e6aee4
Clara Araújo <claragsaraujo@gmail.com>
2025-05-27 12:47:56 -0300

    Update sephora_nlp_full_script.py

--- sephora_nlp_full_script.py
+++ sephora_nlp_full_script.py
@@ -140,7 +140,158 @@ df_reviews4['review_text'] = df_reviews4['review_text'].apply(clean_text)
 df_reviews5['review_text'] = df_reviews5['review_text'].apply(clean_text)
 
 #--------------------Análise exploratória
+# produtos mais populares por avaliações e melhores notas
+most_reviews_products = df_info.sort_values(by= 'reviews', ascending=False).head(20) #refiltrar pós limpeza
+plt.figure(figsize=(12, 6))
+sns.barplot(data= most_reviews_products, x = 'reviews', y='product_name', hue= 'product_name', orient = 'h')
+plt.title('Quantidade de avaliações por produto')
+plt.xlabel('Número de avaliações')
+plt.ylabel('Produtos')
+plt.tight_layout()
+plt.show()
+
+top_products = df_info.sort_values(by= 'rating', ascending=False).head(20) #refiltrar pós limpeza
+plt.figure(figsize=(12, 6))
+sns.barplot(data= top_products, x = 'rating', y='product_name', hue= 'product_name', orient = 'h')
+plt.title('Melhores produtos por avaliação')
+plt.xlabel('Avaliação')
+plt.ylabel('Produtos')
+plt.tight_layout()
+plt.show()
+
+# quais as marcas tem mais avaliações e quais sao melhores avaliadas
+most_reviews_brands = df_info.groupby(['brand_id', 'brand_name']).agg({'reviews': 'sum'}).reset_index()
+most_reviews_brands = most_reviews_brands.sort_values(by= 'reviews', ascending=False).head(20)
+plt.figure(figsize=(12, 6))
+sns.barplot(data= most_reviews_brands, x = 'reviews', y='brand_name', hue='brand_name',orient = 'h', errorbar=None)
+plt.title('Quantidade de avaliações por marca')
+plt.xlabel('Número de avaliações')
+plt.ylabel('Marcas')
+plt.tight_layout()
+plt.show()
+
+top_brands = df_info.groupby(['brand_id', 'brand_name']).agg({'rating': 'mean'}).reset_index()
+top_brands = top_brands.sort_values(by='rating', ascending= False).head(20)
+plt.figure(figsize=(12, 6))
+sns.barplot(data= top_brands, x = 'rating', y='brand_name', hue='brand_name', orient = 'h')
+plt.title('Melhores marcas por avaliação')
+plt.xlabel('Avaliação')
+plt.ylabel('Marcas')
+plt.tight_layout()
+plt.show()
+
+# distribuição de avaliações 
+plt.figure(figsize=(10, 6))
+sns.histplot(df_info['rating'], bins= 5)
+plt.title('Distribuição de avaliações')
+plt.xlabel('Avaliação')
+plt.ylabel('Frequência')
+plt.tight_layout()
+plt.show()
+
+# nuvem de palavras
+stopwords_custom = set(STOPWORDS).union(set(['im', 'ive', 'like', 'it', 'its', 'that', 'this', 'and', 'the', 'to', 'a', 'of', 'is', 'for', 'in', 'on', 'with', 'dont', 'make', 'didnt', 'cant']))
+
+all_text = ' '.join(df_reviews1['review_text'].astype(str)) + ' ' + \
+           ' '.join(df_reviews2['review_text'].astype(str)) + ' ' + \
+           ' '.join(df_reviews3['review_text'].astype(str)) + ' ' + \
+           ' '.join(df_reviews4['review_text'].astype(str)) + ' ' + \
+           ' '.join(df_reviews5['review_text'].astype(str))
+words = [word.lower() for word in all_text.split() if word.lower() not in stopwords_custom]
+word_freq = Counter(words)
+wordcloud = WordCloud(width=800, 
+                      height=400, 
+                      background_color='white', 
+                      colormap='magma',
+                      stopwords=stopwords_custom)
+wordcloud.generate_from_frequencies(word_freq)
+plt.figure(figsize=(10, 6))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title('Nuvem de palavras')
+plt.show()
+
+# analise temporal
+sns.lineplot(data=df_reviews1, x='submission_time', y='rating')
+plt.title('Avaliação ao longo do tempo')
+plt.xlabel('Ano')
+plt.ylabel('Avaliação')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+
 
 #--------------------Natural Language Processing
+# análise de sentimentos
+def get_sentiment(df):
+    sentiment = []
+    scores = []
+    sentiment_pipeline = pipeline('sentiment-analysis', model = 'cardiffnlp/twitter-roberta-base-sentiment')
+    label_mapping = {
+        'LABEL_0': 'negative',
+        'LABEL_1': 'neutral',
+        'LABEL_2': 'positive'
+    }
+    for text in tqdm(df['review_text']):
+        result = sentiment_pipeline(text)
+        label = result[0]['label']
+        score = result[0]['score']
+        mapped_label = label_mapping[label]
+        sentiment.append(mapped_label)
+        scores.append(score)
+    df['sentiment_score'] = scores
+    df['sentiment'] = sentiment
+    return df
+df_reviews5 = get_sentiment(df_reviews5) #apenas um df_reviews para poupar tempo
+
+# verificando os sentimentos analisados
+print('\nSentimentos após NLP:')
+print(df_reviews5[['review_text', 'sentiment', 'sentiment_score']].head())
+
+# descobrir os temas mais comuns (utilizando df_reviews5)
+def get_topics(df, text_column = 'review_text', language = 'english', min_topic_size=50, n_gram_range=(1,2), n_top_words=10):
+    texts = df[text_column].astype(str).tolist()
+    vectorizer_model = CountVectorizer(
+        stop_words = 'english',
+        ngram_range=n_gram_range
+    )
+    topic_model = BERTopic(
+        language=language,
+        min_topic_size=min_topic_size,
+        vectorizer_model=vectorizer_model,
+        verbose = True
+    )
+    topics, probs = topic_model.fit_transform(texts)
+
+    topic_words = {}
+    for topic_num in set(topics):
+        if topic_num == -1:
+            topic_words[topic_num] = "Outliers"
+        else:
+            words = topic_model.get_topic(topic_num)
+            top_words = [word for word, _ in words[:n_top_words]]
+            topic_words[topic_num] = ", ".join(top_words)
+
+    df['topic'] = topics
+    df['topic_text'] = [topic_words[t] for t in topics]
+    return df, topic_model, topics, probs
+
+df_reviews5, topic_model, topics, probs = get_topics(df_reviews5)
+topic_model.visualize_topics()
+
+# quais as palavras mais comuns em cada tipo de avaliação (positiva, negativa, neutra)
+def common_words_by_sentiment(df, sentiment_column='sentiment', text_column = 'review_text'):
+    sentiment_groups = df.groupby(sentiment_column)[text_column].apply(lambda x: ' '.join(x)).reset_index()
+    common_words ={}
+
+    for _, row in sentiment_groups.iterrows():
+        words = row[text_column].split()
+        word_counts = Counter(words)
+        common_words[row[sentiment_column]] = word_counts.most_common(10)
+    
+    return common_words
+
+common_words_review5 = common_words_by_sentiment(df_reviews5, sentiment_column = 'sentiment', text_column='review_text')
+
 
 #--------------------Visualizações
