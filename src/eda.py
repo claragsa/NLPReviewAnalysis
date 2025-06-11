753d3aee015c3b97aace70b19d0b8842a9c3bb4f
Clara Araújo <claragsaraujo@gmail.com>
2025-06-11 16:58:36 -0300

    Update eda.py

--- src/eda.py
+++ src/eda.py
@@ -7,7 +7,7 @@ def plot_most_reviewed_products(df_info):
     most_reviews_products = df_info.sort_values(by= 'reviews', ascending=False).head(20)
     plt.figure(figsize=(12, 6))
     plot_most_reviews_product = sns.barplot(data= most_reviews_products, x = 'reviews', y='product_name', hue= 'product_name', orient = 'h')
-    plt.title('Number of reviews per product')
+    plt.title('Top 20 most reviewed products')
     plt.xlabel('Number of reviews')
     plt.ylabel('Products')
     plt.tight_layout()
@@ -18,7 +18,7 @@ def plot_top_popular_products(df_info):
     top_products = df_info.sort_values(by= 'rating', ascending=False).head(20) 
     plt.figure(figsize=(12, 6))
     plot_top_products = sns.barplot(data= top_products, x = 'rating', y='product_name', hue= 'product_name', orient = 'h')
-    plt.title('Top products per rating')
+    plt.title('Top 20 highest rating products')
     plt.xlabel('Rating')
     plt.ylabel('Products')
     plt.tight_layout()
@@ -30,7 +30,7 @@ def plot_most_reviewed_brands (df_info):
     most_reviews_brands = most_reviews_brands.sort_values(by= 'reviews', ascending=False).head(20)
     plt.figure(figsize=(12, 6))
     plot_most_reviewed_brands = sns.barplot(data= most_reviews_brands, x = 'reviews', y='brand_name', hue='brand_name',orient = 'h', errorbar=None)
-    plt.title('Number of reviews per brand')
+    plt.title('Top 20 most reviewed brands')
     plt.xlabel('Number of reviews')
     plt.ylabel('Brands')
     plt.tight_layout()
@@ -43,7 +43,7 @@ def plot_most_popular_brands(df_info):
     top_brands = top_brands.sort_values(by='rating', ascending= False).head(20)
     plt.figure(figsize=(12, 6))
     plot_top_brands = sns.barplot(data= top_brands, x = 'rating', y='brand_name', hue='brand_name', orient = 'h')
-    plt.title('Top brands per rating')
+    plt.title('Top 20 highest rating brands')
     plt.xlabel('Rating')
     plt.ylabel('Brands')
     plt.tight_layout()
@@ -52,20 +52,18 @@ def plot_most_popular_brands(df_info):
 
 def plot_ratings_distribuition(df_info):
     plt.figure(figsize=(10, 6))
-    rating_distribuition = sns.histplot(df_info['rating'], bins= 5)
-    plt.title('Rating Distribuition')
+    rating_distribution = sns.histplot(df_info['rating'], bins= 5)
+    plt.title('Rating Distribution')
     plt.xlabel('Rating')
-    plt.ylabel('Frequence')
+    plt.ylabel('Frequency')
     plt.tight_layout()
     plt.show()
-    return rating_distribuition
+    return rating_distribution
 
-def plot_word_cloud(dfs, column = 'review_text'):
+def plot_word_cloud(df, column = 'cleaned_text'):
     stopwords_custom = set(STOPWORDS).union(set(['im', 'ive', 'like', 'it', 'its', 'that', 'this', 'and', 'the', 'to', 'a', 'of', 'is', 'for', 'in', 'on', 'with', 'dont', 'make', 'didnt', 'cant']))
 
-    all_text = ' '.join(
-        ' '.join(df[column].astype(str) for df in dfs if column in df.columns)
-    )
+    all_text = ' '.join(df[column].astype(str))
     words = [word.lower() for word in all_text.split() if word.lower() not in stopwords_custom]
     word_freq = Counter(words)
 
@@ -84,9 +82,9 @@ def plot_word_cloud(dfs, column = 'review_text'):
 
 def plot_temporal_analysis(df):
     plot_temporal_analysis = sns.lineplot(data=df, x='submission_time', y='rating')
-    plt.title('Avaliação ao longo do tempo')
-    plt.xlabel('Ano')
-    plt.ylabel('Avaliação')
+    plt.title('Rating trend over time')
+    plt.xlabel('Year')
+    plt.ylabel('Rating')
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
