ca9a73202a769c3e36d635701ebc80c1490f6de4
Clara Araújo <claragsaraujo@gmail.com>
2025-06-11 16:57:26 -0300

    Update data_loading.py

--- src/data_loading.py
+++ src/data_loading.py
@@ -3,11 +3,11 @@ import pandas as pd
 def load_data():
     df_info = pd.read_csv(r'data\product_info.csv')
     df_reviews_files = [
-        'data\reviews_0-250.csv',
-        'data\reviews_250-500.csv',
-        'data\reviews_500-750.csv',
-        'data\reviews_750-1250.csv',
-        'data\reviews_1250-end.csv'
+        r'data\reviews_0-250.csv',
+        r'data\reviews_250-500.csv',
+        r'data\reviews_500-750.csv',
+        r'data\reviews_750-1250.csv',
+        r'data\reviews_1250-end.csv'
     ]
     df_reviews = [pd.read_csv(f) for f in df_reviews_files]
     return df_info, df_reviews
