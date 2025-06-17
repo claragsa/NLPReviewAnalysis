d2b1164019fe2731d1131f8ed9978b40486de3dd
Clara Araújo <claragsaraujo@gmail.com>
2025-06-17 14:44:11 -0300

    Update nlp_analysis.py

--- src/nlp_analysis.py
+++ src/nlp_analysis.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from bertopic import BERTopic
 from collections import Counter
 
-def get_sentiment(df):
+def get_sentiment(df, text_column='cleaned_text'):
     sentiment = []
     scores = []
     sentiment_pipeline = pipeline('sentiment-analysis', model = 'cardiffnlp/twitter-roberta-base-sentiment')
@@ -13,7 +13,7 @@ def get_sentiment(df):
         'LABEL_1': 'neutral',
         'LABEL_2': 'positive'
     }
-    for text in tqdm(df['review_text']):
+    for text in tqdm(df[text_column]):
         result = sentiment_pipeline(text)
         label = result[0]['label']
         score = result[0]['score']
@@ -24,7 +24,7 @@ def get_sentiment(df):
     df['sentiment'] = sentiment
     return df
 
-def get_topics(df, text_column = 'review_text', language = 'english', min_topic_size=50, n_gram_range=(1,2), n_top_words=10):
+def get_topics(df, text_column = 'cleaned_text', language = 'english', min_topic_size=50, n_gram_range=(1,2), n_top_words=10):
     texts = df[text_column].astype(str).tolist()
     vectorizer_model = CountVectorizer(
         stop_words = 'english',
@@ -51,7 +51,7 @@ def get_topics(df, text_column = 'review_text', language = 'english', min_topic_
     df['topic_text'] = [topic_words[t] for t in topics]
     return df, topic_model, topics, probs
 
-def common_words_by_sentiment(df, sentiment_column='sentiment', text_column = 'review_text'):
+def common_words_by_sentiment(df, sentiment_column='sentiment', text_column = 'cleaned_text'):
     sentiment_groups = df.groupby(sentiment_column)[text_column].apply(lambda x: ' '.join(x)).reset_index()
     common_words ={}
 
